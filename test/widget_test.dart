9f33b108aa539b272fe39cee873877ef4ee7b8f8
Anshika Singh <125690747+Anshi1102@users.noreply.github.com>
2024-11-28 20:37:16 +0530

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:lalit_mohan_bhatt_vinove_company/main.dart';
+import 'package:anshika_singh_vinove_company/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
